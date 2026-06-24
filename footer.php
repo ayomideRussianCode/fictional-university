41a40fe0bc5120af188c5ecc7dd7329f285b6a21
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-06-24 23:36:18 +0100

    live search

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
                 <div class="site-footer__col-three">
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
-                      
+
                         <ul>
                             <li><a href="#">Legal</a></li>
                             <li><a href="<?php echo site_url('privacy-policy'); ?>">Privacy Policy
@@ -59,6 +59,22 @@
         </div>
     </div>
 </footer>
+
+<div class="search-overlay">
+    <div class="search-overlay__top">
+        <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div id="search-overlay__results">
+
+    </div>
+</div>
 <?php wp_footer(); ?>
 </body>
 
