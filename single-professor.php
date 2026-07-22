34747d85f8aefb26eebefee4e321531f7158b8d5
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-07-22 20:31:40 +0100

    allow user like content

--- single-professor.php
+++ single-professor.php
@@ -4,8 +4,8 @@ get_header();
 while (have_posts()) {
     the_post();
     pageBanner();
-     ?>
-    
+?>
+
     <div class="container container--narrow page-section">
         <div class="generic-content">
             <div class="row-group">
@@ -14,6 +14,41 @@ while (have_posts()) {
                     ?>
                 </div>
                 <div class="two-thirds">
+                    <?php 
+                    $likeCount = new WP_Query(array(
+                        'post_type' => 'like',
+                        'meta_query' => array(
+                            array(
+                                'key' => 'liked_professor_id',
+                                'compare' => '=',
+                                'value' => get_the_ID()
+                            )
+                        )
+                    ));
+
+                    $existStatus = 'no';
+
+                      $existQuery = new WP_Query(array(
+                        'author' => get_current_user_id(),
+                        'post_type' => 'like',
+                        'meta_query' => array(
+                            array(
+                                'key' => 'liked_professor_id',
+                                'compare' => '=',
+                                'value' => get_the_ID()
+                            )
+                        )
+                    ));
+
+                    if($existQuery->found_posts) {
+                        $existStatus = 'yes';
+                    }
+                    ?>
+                    <span class="like-box" data-exists="<?php echo $existStatus; ?>" data-professor="<?php the_ID(); ?>" data-like="<?php echo $existQuery->posts[0]->ID; ?>">
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                        <i class="fa fa-heart" aria-hidden="true"></i>
+                        <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+                    </span>
                     <?php
                     the_content();
                     ?>
@@ -42,4 +77,3 @@ while (have_posts()) {
 <?php }
 get_footer();
 ?>
-
---
+++ src/modules/Like.js
@@ -0,0 +1,44 @@
+import $ from "jquery"
+
+class Like{
+    constructor(){
+        this.events();
+    }
+
+    events(){
+        $(".like-box").on("click", this.ourClickDispatcher.bind(this));
+    }
+
+
+    //methods
+
+    ourClickDispatcher(e){
+        var currentLikeBox = $(e.target).closest(".like-box");
+
+        if(currentLikeBox.data("exists") == "yes"){
+            this.deleteLike();
+    } else {
+        this.createLike();
+    }
+}
+
+    createLike() {
+       $.ajax({
+        url: universityData.root_url + "/wp-json/university/v1/manageLike",
+        type: "POST",
+        success: (response) => {console.log("Congrats, you created a new like!"); console.log(response);},
+        error: (response) => { console.log("Sorry, you could not create a new like."); console.log(response);},
+        data: {professorId: universityData.professor_id},
+       });
+    }
+
+    deleteLike(){
+       $.ajax({
+        beforeSend: (xhr) => {
+            xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+        }
+       })
+    }
+}
+
+export default Like;
